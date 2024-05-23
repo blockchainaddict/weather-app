56041b7b5bbc3f76b7748ff9d0108be0eff9ef57
blockchainaddict <vazquezbare@gmail.com>
2024-05-23 18:36:05 +0200

    color codeing fixed

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -3,12 +3,12 @@ import getWeather from "../../services/weatherService";
 import useLocation from "../../services/useLocation";
 import Modal from "../Modal/Modal";
 
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
 function Weather() {
   const [weatherData, setWeatherData] = useState({});
-  const [ currentIndex, setCurrentIndex ] = useState(0);
-  const [ cardStyle, setCardStyle ] = useState({
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardStyle, setCardStyle] = useState({
     tempBoxShadow: null,
     windBoxShadow: null,
     rainBoxShadow: null,
@@ -22,84 +22,86 @@ function Weather() {
   const currentTime = new Date();
   const localISOTime = format(currentTime, "yyyy-MM-dd'T'HH:00");
 
-  console.log('TIME', localISOTime);
-
-  const toggleModal = () =>{
+  const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
-  }
+  };
 
-   // Function to close the modal from within the child
-   const closeModal = () => {
+  // Function to close the modal from within the child
+  const closeModal = () => {
     setIsModalOpen(false);
   };
 
   function getTempShadowColor(temp) {
-    console.log('temp - - - - - ', temp);
-
     if (temp < 5) {
-      console.log('less than 5');
-      return 'white';
+      return "white";
     } else if (temp < 10) {
-      console.log('less than 10');
-      return 'blue';
+      return "blue";
     } else if (temp < 15) {
-      console.log('less than 15');
-      return 'violet';
+      return "violet";
+    } else if (temp < 20) {
+      return "green";
     } else {
-      console.log('more than 15');
-      return 'orange';
+      return "orange";
     }
   }
 
   function getWindShadowColor(windSpeed) {
+
     if (windSpeed < 5) {
-      return 'green'; // Low wind speeds
+      return "green"; // Low wind speeds
     } else if (windSpeed < 10) {
-      return 'yellow'; // Moderate wind speeds
+      return "yellow"; // Moderate wind speeds
     } else if (windSpeed < 15) {
-      return 'orange'; // High wind speeds
+      return "orange"; // High wind speeds
     } else {
-      return 'violet'; // Very high wind speeds
+      return "violet"; // Very high wind speeds
     }
   }
 
   function getRainShadowColor(rain) {
-    if (rain < 1) {
-      return 'cornflowerBlue'; // Low wind speeds
-    } else if (rain < 10) {
-      return 'yellow'; // Moderate wind speeds
-    } else if (rain < 30) {
-      return 'orange'; // High wind speeds
+    console.log('RAIN:', rain);
+
+    if (rain == 0) {
+      return "cornflowerBlue";
+    } else if (rain < 0.3) {
+      return "lightblue";
     } else {
-      return 'violet'; // Very high wind speeds
+      return "violet"; // Very high wind speeds
     }
   }
 
   useEffect(() => {
     const fetchWeather = async () => {
       try {
-        const data = await getWeather(location.latitude, location.longitude);
+        const data = await getWeather(location?.latitude, location?.longitude);
         setWeatherData(data);
-        
-        const index = data.hourly.time.indexOf(localISOTime);
-        console.log('index', index);
-        setCurrentIndex(index); 
+
+        const index = data.hourly?.time?.indexOf(localISOTime);
+        setCurrentIndex(index);
         // setRain(data.hourly.rain);
 
         // Get the appropriate box-shadow color based on wind speed
-        const tempBoxShadowColor = getTempShadowColor(weatherData?.current_weather?.temperature);
-        console.log('tempBoxShadowColor', tempBoxShadowColor);
-        const windBoxShadowColor = getWindShadowColor(data?.hourly?.windspeed_10m[index]);
-        const rainBoxShadowColor = getRainShadowColor(data?.hourly?.rain[index]);
+        const tempBoxShadowColor = getTempShadowColor(
+          weatherData?.current_weather?.temperature
+        );
+        const windBoxShadowColor = getWindShadowColor(
+          data?.hourly?.windspeed_10m[index]
+        );
+        const rainBoxShadowColor = getRainShadowColor(
+          data?.hourly?.rain[index]
+        );
 
         // Dynamic style for the weather card
-        setCardStyle ((p)=>{
-          return {...p, tempBoxShadow: `0px 0px 10px ${tempBoxShadowColor}`, windBoxSadow: `0px 0px 10px ${windBoxShadowColor}`, rainBoxShadow: `0px 0px 10px ${rainBoxShadowColor}`};
-        })
+        setCardStyle((p) => {
+          return {
+            ...p,
+            tempBoxShadow: `0px 0px 10px ${tempBoxShadowColor}`,
+            windBoxShadow: `0px 0px 10px ${windBoxShadowColor}`,
+            rainBoxShadow: `0px 0px 10px ${rainBoxShadowColor}`,
+          };
+        });
 
         console.log("data", data);
-        // console.log('TIME OBJ = = = = ', data.hourly.time);
-
       } catch (error) {
         console.log(error);
       }
@@ -114,26 +116,41 @@ function Weather() {
         {error && <p>Error: {error.message}</p>}
         {weatherData ? (
           <div className="weather-container" onClick={toggleModal}>
-            <div className="weather-card" style={{boxShadow: cardStyle.tempBoxShadow}}>
+            <div
+              className="weather-card"
+              style={{ boxShadow: cardStyle.tempBoxShadow }}
+            >
               <p className="weather-info-text">
                 {weatherData?.current_weather?.temperature} °C
               </p>
               <sub>Temperature</sub>
             </div>
 
-            <div className="weather-card" style={{boxShadow: cardStyle.windBoxShadow}} onClick={toggleModal}>
+            <div
+              className="weather-card"
+              style={{ boxShadow: cardStyle.windBoxShadow }}
+              onClick={toggleModal}
+            >
               <p className="weather-info-text">
-                {weatherData?.hourly?.windspeed_10m[currentIndex]} m/s
+                {weatherData?.hourly?.windspeed_10m[currentIndex]} km/h
               </p>
               <sub>Winds</sub>
             </div>
 
-            <div className="weather-card" style={{boxShadow: cardStyle.rainBoxShadow}} onClick={toggleModal}>
+            <div
+              className="weather-card"
+              style={{ boxShadow: cardStyle.rainBoxShadow }}
+              onClick={toggleModal}
+            >
               <p className="weather-info-text">
-                {weatherData?.hourly?.rain[currentIndex] ? "It will rain" : "Clear skies"}
+                {weatherData?.hourly?.rain[currentIndex]
+                  ? "It might rain"
+                  : "Clear skies"}
               </p>
               <p className="weather-info-text">
-                {weatherData?.hourly?.snowfall[currentIndex] ? "It will snow" : null}
+                {weatherData?.hourly?.snowfall[currentIndex]
+                  ? "It might snow"
+                  : null}
               </p>
               <sub>Skies</sub>
             </div>
@@ -143,7 +160,7 @@ function Weather() {
         )}
       </div>
 
-        {isModalOpen && <Modal title={"Temperature"} onClose={closeModal}/>}
+      {isModalOpen && <Modal title={"Temperature"} onClose={closeModal} />}
 
       <div className="weather-background"></div>
     </div>
